edd0fda13a9f639dd83cfddb55f30d5968086ea8
jshuadvd <jshuadvd@icloud.com>
2016-01-26 23:45:19 -0800

    Fill tests

--- Basics/Step 5/build/build/source/build.java
+++ Basics/Step 5/build/build/source/build.java
@@ -18,22 +18,32 @@ public void setup() {
   
 
   rotate(radians(10));
+  fill(255);
+  rect(400, 100, 50, 50);
   noFill();
   rect(0, 0, 600, 600);
 
   rotate(radians(10));
+  fill(255);
+  rect(400, 100, 50, 50);
   noFill();
   rect(0, 0, 600, 600);
 
   rotate(radians(10));
+  fill(255);
+  rect(400, 100, 50, 50);
   noFill();
   rect(0, 0, 600, 600);
 
   rotate(radians(10));
+  fill(255);
+  rect(400, 100, 50, 50);
   noFill();
   rect(0, 0, 600, 600);
 
   rotate(radians(10));
+  fill(255);
+  rect(400, 100, 50, 50);
   noFill();
   rect(0, 0, 600, 600);
 }
