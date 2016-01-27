77779587621c5f9d619082865b133fbf20076ea4
jshuadvd <jshuadvd@icloud.com>
2016-01-26 23:59:50 -0800

    Row of rects

---
+++ Matrix/Step 2/build/build/source/build.java
@@ -0,0 +1,62 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class build extends PApplet {
+
+public void setup() {
+
+  
+
+  pushMatrix();
+      translate(100, 100);
+      rotate(radians(45));
+      rect(0, 0, 50, 50);
+  popMatrix();
+
+  pushMatrix();
+      translate(200, 100);
+      rotate(radians(45));
+      rect(0, 0, 50, 50);
+  popMatrix();
+
+  pushMatrix();
+      translate(300, 100);
+      rotate(radians(45));
+      rect(0, 0, 50, 50);
+  popMatrix();
+
+  pushMatrix();
+      translate(400, 100);
+      rotate(radians(45));
+      rect(0, 0, 50, 50);
+  popMatrix();
+
+  pushMatrix();
+      translate(500, 100);
+      rotate(radians(45));
+      rect(0, 0, 50, 50);
+  popMatrix();
+
+
+}
+  public void settings() {  size(600, 600); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "build" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
